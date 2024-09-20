71b795023399404f7a091839872a8d3e56af83f9
bastianpeitersenEM <bastian.peitersen@energymachines.com>
2024-09-20 17:20:12 +0200

    others

--- General Modules/Weights_update_example.jl
+++ General Modules/Weights_update_example.jl
@@ -17,7 +17,7 @@ matrix_data = Matrix(df)
 #matrix_data = hcat(matrix_data, 0.0*ones(size(matrix_data, 1)))
 
 #We choose monthly 95% cvar to be leq 0.05 And arrive almost at the 60-40 portfolio
-optimal_weights = optimize_portfolio_CVaR_const(-matrix_data[1:end,2:end],0.05,false,0.95)
+optimal_weights = optimize_portfolio_CVaR_const(-matrix_data[1:end,2:end],0.05,0.95,false)
 #
 #=
 Now assume we start with an equal weighted portfolio
@@ -27,14 +27,23 @@ n = size(matrix_data,2)-1
 println(optimal_weights[1:n])
 
 current_weights = [0.34,0.33,0.33,0,0,0]
-new_weights, updates = update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.02,0.95)
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.02,0.95)
 #we now arrive at a quite different solution.
 #Selling clearly hurts so in order to live with the loss incurred by selling we have to invest it into the safe bonds
 println(new_weights[1:n]-new_weights[n+1 : 2*n])
 #If we lower the trading costs we get closer and closer to the 60-40 portfolio
-new_weights, updates = update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.01,0.95)
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.01,0.95)
 println(new_weights[1:n]-new_weights[n+1 : 2*n])
-new_weights, updates = update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.005,0.95)
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.005,0.95)
+println(new_weights[1:n]-new_weights[n+1 : 2*n])
+
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.0005,0.95)
+println(new_weights[1:n]-new_weights[n+1 : 2*n])
+
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.0002,0.95)
+println(new_weights[1:n]-new_weights[n+1 : 2*n])
+
+new_weights, updates = CVaR_functions.update_portfolio_weights(current_weights,-matrix_data[1:end,2:end],0.05,9999,false,0.0,0.95)
 println(new_weights[1:n]-new_weights[n+1 : 2*n])
 
 
